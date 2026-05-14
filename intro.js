7b03988bcac982bb654e332e9ef5f970ea945a5c
shehuyusuf <shehuyusuf124@gmail.com>
2026-05-14 18:04:03 +0100

    Initial commit : Hello World Project

---
+++ intro.js
@@ -0,0 +1,19 @@
+const name = "Shehu Yusuf";
+
+console.log("My Name is:", name);
+
+let age = 10;
+
+age = 24;
+
+console.log("I am", age, "year old");
+
+
+function program(language){
+    return "My favorite programming language is: " + language
+}
+
+console.log(program("Python"));
+
+
+
